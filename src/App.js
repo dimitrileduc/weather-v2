073ee91e4366390b994a35a9717b6e8b459ae7b0
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-04-18 20:51:56 +0200

    lat lon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { useState, useEffect } from "react";
+import Moment from "moment";
 
 import "./App.css";
 
 function App() {
   const [weather, setWeather] = useState({});
   const [locations, setLocations] = useState("london");
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+
   const [photos, setPhotos] = useState([]);
   const [currentT, setCurrentT] = useState();
   const [currentWeather, setCurrentWeather] = useState();
+  const [day, setDay] = useState();
+  const [weekT, setWeekT] = useState();
+
+  const formatDate = Moment().format("[Today ] ");
+  const tommorowDate = Moment()
+    .add(1, "days")
+    .format("ddd DD MMMM ")
+    .toString();
+  const j2Date = Moment().add(2, "days").format("ddd DD MMMM ").toString();
+  const j3Date = Moment().add(3, "days").format("ddd DD MMMM ").toString();
+  const j4Date = Moment().add(4, "days").format("ddd DD MMMM ").toString();
+  const j5Date = Moment().add(5, "days").format("ddd DD MMMM ").toString();
+  const j6Date = Moment().add(6, "days").format("ddd DD MMMM ").toString();
+
   useEffect(() => {
     ifClicked();
   }, []);
 
   function ifClicked() {
     fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${locations}&APPID=c728df12326ce2d393585d06d1f41d26&units=metric`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${locations}&limit=1&appid=c728df12326ce2d393585d06d1f41d26&units=metric`
     )
       .then((res) => {
         if (res.ok) {
@@ -22,19 +40,45 @@ function App() {
           return res.json();
         } else {
           if (res.status === 404) {
-            return alert("Oops, there seems to be an error!(wrong location)");
+            //return alert("Oops, there seems to be an error!(wrong location)");
           }
-          alert("Oops, there seems to be an error!");
-          throw new Error("You have an error");
+          //alert("Oops, there seems to be an error!");
+          //throw new Error("You have an error");
         }
       })
       .then((object) => {
-        setWeather(object);
-        console.log(object);
-        setCurrentT(object.list[0].main.temp);
-        setCurrentWeather(object.list[0].weather[0].main);
+        //console.log(object[0].lat)
+        setLat(object[0].lat);
+        setLon(object[0].lon);
+
+        fetch(
+          `http://api.openweathermap.org/data/2.5/forecast?q=${locations}&APPID=c728df12326ce2d393585d06d1f41d26&units=metric`
+        )
+          .then((res) => {
+            if (res.ok) {
+              console.log(res.status);
+              return res.json();
+            } else {
+              if (res.status === 404) {
+                return alert(
+                  "Oops, there seems to be an error!(wrong location)"
+                );
+              }
+              alert("Oops, there seems to be an error!");
+              throw new Error("You have an error");
+            }
+          })
+          .then((object) => {
+            setWeather(object);
+            console.log(object);
+            //setCurrentT(object.list[0].main.temp);
+            //setCurrentWeather(object.list[0].weather[0].main);
+            //console.log(tommorowDate)
+          })
+          .catch((error) => console.log(error));
       })
       .catch((error) => console.log(error));
+
     fetch(
       `https://api.unsplash.com/search/photos?query=${locations}&client_id=I8PNeqzqy1xX9iUVSU8DC_FdGnrQUSomU1c9zSOaEww`
     )
@@ -67,8 +111,14 @@ function App() {
           </button>
         </div>
         <div className="app__data">
-          <p className="temp">Current Temperature: {currentT}</p>
-          <p className="temp">Current Weather: {currentWeather}</p>
+          <h3>{formatDate}</h3>
+
+          <h3>{tommorowDate}</h3>
+          <h3>{j2Date}</h3>
+          <h3>{j3Date}</h3>
+          <h3>{j4Date}</h3>
+          <h3>{j5Date}</h3>
+          <h3>{j6Date}</h3>
         </div>
         <img className="app__image" src={photos} alt="" />
       </div>
@@ -77,3 +127,8 @@ function App() {
 }
 
 export default App;
+
+/*
+ <p className="temp">Current Temperature: {currentT}</p>
+          <p className="temp">Current Weather: {currentWeather}</p>
+          */
