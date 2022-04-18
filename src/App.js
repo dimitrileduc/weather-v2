4f4f9f07a5c4567bdd309857389970d688c685de
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-04-18 14:31:08 +0200

    first commit clean workspace

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+function App() {
+const [weather, setWeather] = useState({});
+const [locations, setLocations] = useState("london");
+const [photos, setPhotos] = useState([]);
+return <div>Let's create a weather app!!</div>;
+}
+export default App;
